750dd8dd29dca783a1e845fbdaa88a61b7949f55
Emy <emily.galeas@epn.edu.ec>
2025-04-30 17:02:42 -0500

    ejercicio 1

--- src/Ejercicio1/Curso.java
+++ src/Ejercicio1/Curso.java
@@ -1,25 +1,25 @@
 package Ejercicio1;
-
+//declarar atributos
 public class Curso {
     public String nombre;
     public String codigo;
     public int creditos;
 
+    //crear metodo constructor
     public Curso(String nombre, String codigo, int creditos) {
         this.nombre = nombre;
         this.codigo = codigo;
         this.creditos = creditos;
     }
 
+    //crear metodo para mostrar info
     public void mostrarInfo(){
-        System.out.println("\nMostrando información del curso...");
-        System.out.printf("----------------------------------------------------");
-        System.out.println("\nNombre del curso: " + nombre);
+        System.out.println("Nombre del curso: " + nombre);
         System.out.println("Código del curso: " + codigo);
         System.out.println("El curso otorga: " + creditos + " créditos.");
-        System.out.printf("----------------------------------------------------");
     }
 
+    //crear metodo para actualizar creditos
     public void actualizarCreditos(int nuevosCreditos){
         this.creditos = nuevosCreditos;
         System.out.println("El número de créditos se ha actualizado a: " + nuevosCreditos);
--- src/Ejercicio1/CursosPrograma.java
+++ src/Ejercicio1/CursosPrograma.java
@@ -4,23 +4,55 @@ import java.util.Scanner;
 public class CursosPrograma {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Curso curso1 = new Curso("Programación I", "INF101",5);
+        //Crear un arreglo de 3 objetos Curso
+        Curso[] cursos=new Curso[3];
 
+        // Bucle para ingresar los datos de 3 cursos por teclado
+        for (int i = 0; i < 3; i++) {
+            System.out.println("\nIngrese los datos del curso " + (i + 1) + ": ");
+
+            System.out.print("Nombre: ");
+            String nombre = sc.nextLine(); //Leer el nombre del curso
+
+            System.out.print("Código: ");
+            String codigo = sc.nextLine(); //Leer el código del curso
+
+            System.out.print("Créditos: ");
+            int creditos = sc.nextInt(); //Leer los créditos del curso
+
+            sc.nextLine(); // limpiar buffer
+            // Crear un nuevo curso con los datos ingresados y almacenarlo en el arreglo
+            cursos[i] = new Curso(nombre, codigo, creditos);
+        }
+
+        // Variable para controlar el menú (se inicializa distinta de 3 para entrar al bucle)
         int opcion = 1;
+        //Menú principal cuando la opción sea distinta de 3
         while (opcion != 3) {
 
-            System.out.println("\nDigite el número de opción que desee realizar \n1.Mostrar Información del Curso.\n2.Actualizar los créditos que otorga el curso.\n3. Salir. ");
+            System.out.println("\nDigite el número de opción que desee realizar \n1.Mostrar Información de los cursos.\n2.Actualizar los créditos que otorga un curso.\n3. Salir. ");
             System.out.printf("Opción: ");
-            opcion = sc.nextInt();
+            opcion = sc.nextInt(); //leer la opción elegida por el usuario
 
             switch (opcion){
                 case 1:
-                    curso1.mostrarInfo();
+                    System.out.println("\nMostrando información de los cursos disponibles...");
+                    for (int i = 0; i < 3; i++) {
+                        System.out.println("----------------------------------------------------");
+                        cursos[i].mostrarInfo(); //llamar al metodo para mostrar la info de los cursos
+                        System.out.println("----------------------------------------------------");
+                    }
                     break;
                 case 2:
-                    System.out.println("Escriba los créditos que otorga el curso: ");
-                    int nuevosCreditos = sc.nextInt();
-                    curso1.actualizarCreditos(nuevosCreditos);1
+                    System.out.print("Ingrese el número del curso (1-3): ");
+                    int num = sc.nextInt(); //leer el número de curso
+                    if (num >= 1 && num<=3) { //verificar que la opción digitada esté en el rando de 1-3
+                        System.out.print("Escriba los créditos que otorga el curso: ");
+                        int nuevosCreditos = sc.nextInt();
+                        cursos[num-1].actualizarCreditos(nuevosCreditos); //se escribe num-1, porque en un arreglo las posiciones son diferentes (comienzan en 0)
+                    }else{
+                        System.out.println("Número inválido.");
+                    }
                     break;
                 case 3:
                     System.out.println("Saliendo...");
