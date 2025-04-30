4329dfda235459f71962a94e2841b3ab25a0e155
NayelyAyol <nayely.ayol@epn.edu.ec>
2025-04-30 16:03:55 -0500

    Ciclista.java

--- src/Ejercicio4/src/Ciclista.java
+++ src/Ejercicio4/src/Ciclista.java
@@ -1,17 +1,22 @@
+//Se crea la calse Ciclista
 public class Ciclista {
+    //Se declaran los atributos
     public String nombre;
     public int edad, kilometros;
 
+    //Constructor para inicializar los atributos
     public Ciclista(String nombre, int edad, int kilometros){
         this.nombre=nombre;
         this.edad=edad;
         this.kilometros=kilometros;
     }
 
+    //Metodo para mostrar la informacion del ciclista
     public void mostrarInfo(){
         System.out.println("\nNombre: "+nombre+"\nEdad:"+edad+"\nKm recorridos: "+kilometros+"\n");
     }
 
+    //Metodo para actualizar los km recorridos del ciclista
     public void actualizaKm(int km_actualizado){
         this.kilometros=km_actualizado;
     }
